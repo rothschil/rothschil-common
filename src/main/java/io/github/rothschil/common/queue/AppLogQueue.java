1eedd57ad474342a425b070b53584c03c2d69c2b
SamAbram <WCNGS@HOTMAIL.COM>
2025-11-17 10:31:00 +0800

    NONE

--- src/main/java/io/github/rothschil/common/queue/AppLogQueue.java
+++ src/main/java/io/github/rothschil/common/queue/AppLogQueue.java
@@ -1,119 +1,119 @@
-package io.github.rothschil.common.queue;
-
-
-import io.github.rothschil.common.handler.IQueueTaskHandler;
-import io.github.rothschil.common.handler.IntfLog;
-import io.github.rothschil.common.utils.thread.PippinRejectedExecutionHandler;
-import io.github.rothschil.common.utils.thread.PippinThreadFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantLock;
-
-/**
- * 异步处理日志的队列，将需要存储的日志放入这个队列中
- * @author <a href="mailto:WCNGS@QQ.COM">Sam</a>
- * @version 1.0.0
- */
-@Component
-public class AppLogQueue {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AppLogQueue.class);
-
-    private final LinkedBlockingQueue<IntfLog> queue = new LinkedBlockingQueue<>(200);
-
-    @Autowired
-    IQueueTaskHandler iQueueTaskHandler;
-
-    /**
-     * 检查服务是否运行
-     */
-    private volatile boolean running = true;
-
-    private static ThreadPoolExecutor threadPoolExecutor;
-
-    static {
-        int poolSize = 1;
-        // 2、自定义线程名字
-        ThreadFactory threadFactory = new PippinThreadFactory(AppLogQueue.class.getName());
-        // 3、自定义线程池超出容量的拒绝策略
-        RejectedExecutionHandler policy = new PippinRejectedExecutionHandler();
-        threadPoolExecutor = new ThreadPoolExecutor(poolSize, 3, 0,
-                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), threadFactory, policy);
-    }
-
-    public AppLogQueue(){
-
-    }
-
+//package io.github.rothschil.common.queue;
+//
+//
+//import io.github.rothschil.common.handler.IQueueTaskHandler;
+//import io.github.rothschil.common.handler.IntfLog;
+//import io.github.rothschil.common.utils.thread.PippinRejectedExecutionHandler;
+//import io.github.rothschil.common.utils.thread.PippinThreadFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import jakarta.annotation.PostConstruct;
+//import jakarta.annotation.PreDestroy;
+//import java.util.Arrays;
+//import java.util.List;
+//import java.util.concurrent.*;
+//import java.util.concurrent.locks.ReentrantLock;
+//
+///**
+// * 异步处理日志的队列，将需要存储的日志放入这个队列中
+// * @author <a href="mailto:WCNGS@QQ.COM">Sam</a>
+// * @version 1.0.0
+// */
+//@Component
+//public class AppLogQueue {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(AppLogQueue.class);
+//
+//    private final LinkedBlockingQueue<IntfLog> queue = new LinkedBlockingQueue<>(200);
+//
+//    @Autowired
+//    IQueueTaskHandler iQueueTaskHandler;
+//
 //    /**
-//     * 线程状态
+//     * 检查服务是否运行
 //     */
-//    private Future<?> threadStatus = null;
-
-    @PostConstruct
-    public void init(){
-//        threadStatus  = threadPoolExecutor.submit(() -> {
-//            while(running){
-//                // 队列中不存在元素 则不处理
-//                if(!queue.isEmpty()){
-//                }
-//            }
-//        });
-    }
-
-    @PreDestroy
-    public void destroys() {
-        running = false;
-        threadPoolExecutor.shutdownNow();
-    }
-
-    public void activeService() {
-        running = true;
-        if (threadPoolExecutor.isShutdown()) {
-            init();
-            LOG.info("线程池关闭，重新初始化线程池及任务");
-        }
-//        if (threadStatus.isDone()) {
+//    private volatile boolean running = true;
+//
+//    private static ThreadPoolExecutor threadPoolExecutor;
+//
+//    static {
+//        int poolSize = 1;
+//        // 2、自定义线程名字
+//        ThreadFactory threadFactory = new PippinThreadFactory(AppLogQueue.class.getName());
+//        // 3、自定义线程池超出容量的拒绝策略
+//        RejectedExecutionHandler policy = new PippinRejectedExecutionHandler();
+//        threadPoolExecutor = new ThreadPoolExecutor(poolSize, 3, 0,
+//                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), threadFactory, policy);
+//    }
+//
+//    public AppLogQueue(){
+//
+//    }
+//
+////    /**
+////     * 线程状态
+////     */
+////    private Future<?> threadStatus = null;
+//
+//    @PostConstruct
+//    public void init(){
+////        threadStatus  = threadPoolExecutor.submit(() -> {
+////            while(running){
+////                // 队列中不存在元素 则不处理
+////                if(!queue.isEmpty()){
+////                }
+////            }
+////        });
+//    }
+//
+//    @PreDestroy
+//    public void destroys() {
+//        running = false;
+//        threadPoolExecutor.shutdownNow();
+//    }
+//
+//    public void activeService() {
+//        running = true;
+//        if (threadPoolExecutor.isShutdown()) {
 //            init();
-//            LOG.info("线程池任务结束，重新!");
+//            LOG.info("线程池关闭，重新初始化线程池及任务");
 //        }
-    }
-
-    public void addQueue(IntfLog intfLog){
-//        if(!running){
-//            LOG.warn("service is stop");
-//            return ;
+////        if (threadStatus.isDone()) {
+////            init();
+////            LOG.info("线程池任务结束，重新!");
+////        }
+//    }
+//
+//    public void addQueue(IntfLog intfLog){
+////        if(!running){
+////            LOG.warn("service is stop");
+////            return ;
+////        }
+//        final ReentrantLock putLock = new ReentrantLock();
+//        putLock.lock();
+//        try {
+//            boolean isFull = queue.offer(intfLog);
+//            if(!isFull){
+//                LOG.warn("[Failed to add queue,Queue full]");
+//                List lists = Arrays.asList(queue.toArray());
+//                iQueueTaskHandler.processData(lists);
+//                queue.clear();
+//                // 清理完，把刚才漏掉的补上
+//                queue.offer(intfLog);
+//            }
+//        } catch (Exception e) {
+//            LOG.warn("[Exception] {}",e.getMessage());
+//        } finally {
+//            putLock.unlock();
 //        }
-        final ReentrantLock putLock = new ReentrantLock();
-        putLock.lock();
-        try {
-            boolean isFull = queue.offer(intfLog);
-            if(!isFull){
-                LOG.warn("[Failed to add queue,Queue full]");
-                List lists = Arrays.asList(queue.toArray());
-                iQueueTaskHandler.processData(lists);
-                queue.clear();
-                // 清理完，把刚才漏掉的补上
-                queue.offer(intfLog);
-            }
-        } catch (Exception e) {
-            LOG.warn("[Exception] {}",e.getMessage());
-        } finally {
-            putLock.unlock();
-        }
-
-    }
-
-    public boolean empty(){
-        return queue.isEmpty();
-    }
-
-}
+//
+//    }
+//
+//    public boolean empty(){
+//        return queue.isEmpty();
+//    }
+//
+//}
--- src/main/java/io/github/rothschil/common/utils/thread/PippinThreadFactory.java
+++ src/main/java/io/github/rothschil/common/utils/thread/PippinThreadFactory.java
@@ -1,46 +1,47 @@
-package io.github.rothschil.common.utils.thread;
-
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
-
-/**
- * 自定义线程池名字
- *
- * @author <a href="https://github.com/rothschil">Sam</a>
- * @date 20/11/19 16:36
- * @since 1.0.0
- */
-public class PippinThreadFactory implements ThreadFactory {
-
-    private final ThreadGroup threadGroup;
-
-    private final AtomicInteger threadNumber = new AtomicInteger(1);
-    public final String namePrefix;
-
-    public PippinThreadFactory(String name) {
-        SecurityManager s = System.getSecurityManager();
-        threadGroup = (s != null) ? s.getThreadGroup() :
-                Thread.currentThread().getThreadGroup();
-        if (null == name || "".equals(name.trim())) {
-            name = "pool";
-        }
-        AtomicInteger poolNumber = new AtomicInteger(1);
-        namePrefix = name + "-" +
-                poolNumber.getAndIncrement() +
-                "-thread-";
-    }
-
-    @Override
-    public Thread newThread(Runnable r) {
-        Thread t = new Thread(threadGroup, r,
-                namePrefix + threadNumber.getAndIncrement(),
-                0);
-        if (t.isDaemon()) {
-            t.setDaemon(false);
-        }
-        if (t.getPriority() != Thread.NORM_PRIORITY) {
-            t.setPriority(Thread.NORM_PRIORITY);
-        }
-        return t;
-    }
-}
+//package io.github.rothschil.common.utils.thread;
+//
+//import java.util.concurrent.ThreadFactory;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+///**
+// * 自定义线程池名字
+// *
+// * @author <a href="https://github.com/rothschil">Sam</a>
+// * @date 20/11/19 16:36
+// * @since 1.0.0
+// */
+//public class PippinThreadFactory implements ThreadFactory {
+//
+//    private final ThreadGroup threadGroup;
+//
+//    private final AtomicInteger threadNumber = new AtomicInteger(1);
+//    public final String namePrefix;
+//
+//    public PippinThreadFactory(String name) {
+//        System.setSecurityManager(new SecurityManager());
+//        SecurityManager s = System.getSecurityManager();
+//        threadGroup = (s != null) ? s.getThreadGroup() :
+//                Thread.currentThread().getThreadGroup();
+//        if (null == name || "".equals(name.trim())) {
+//            name = "pool";
+//        }
+//        AtomicInteger poolNumber = new AtomicInteger(1);
+//        namePrefix = name + "-" +
+//                poolNumber.getAndIncrement() +
+//                "-thread-";
+//    }
+//
+//    @Override
+//    public Thread newThread(Runnable r) {
+//        Thread t = new Thread(threadGroup, r,
+//                namePrefix + threadNumber.getAndIncrement(),
+//                0);
+//        if (t.isDaemon()) {
+//            t.setDaemon(false);
+//        }
+//        if (t.getPriority() != Thread.NORM_PRIORITY) {
+//            t.setPriority(Thread.NORM_PRIORITY);
+//        }
+//        return t;
+//    }
+//}
